b24e1ef04c668d13ddaef0e5e58c8bc859436be6
kim jae gyoun <worbswo@naver.com>
2022-12-19 16:58:16 +0900

    add Item Class

--- Http/Code/HttpClient.cs
+++ Http/Code/HttpClient.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Http.Code;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,36 +12,7 @@ using System.Windows;
 
 namespace LostArkAction.Code
 {
-    public class Item
-    {
-        public int ItemLevelMin = 0;
-        public int ItemLevelMax = 1700;
-        public int ItemGradeQuality=10;
-        public List<SkillOption> SkillOptions = new List<SkillOption>();
-        public List<EtcOption> EtcOptions = new List<EtcOption>(); 
-        public string Sort= "ITEM_GRADE";
-        public int CategoryCode;
-        public string CharacterClass;
-        public int ItemTier=3;
-        public string ItemGrade="고대";
-        public int PageNo=1;
-        public string SortCondition= "ASC";
-
-    }
-    public class SkillOption
-    {
-        public int FirstOption;
-        public int SecondOption;
-        public int MinValue=0;
-        public int MaxValue=0;
-    }
-    public class EtcOption
-    {
-        public int FirstOption;
-        public int SecondOption;
-        public int MinValue=0;
-        public int MaxValue=500;
-    }
+   
 
     public class HttpClient2
     {
---
+++ Http/Code/Item.cs
@@ -0,0 +1,26 @@
+﻿using LostArkAction.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Http.Code
+{
+    public class Item
+    {
+        public int ItemLevelMin = 0;
+        public int ItemLevelMax = 1700;
+        public int ItemGradeQuality = 10;
+        public List<SkillOption> SkillOptions = new List<SkillOption>();
+        public List<EtcOption> EtcOptions = new List<EtcOption>();
+        public string Sort = "ITEM_GRADE";
+        public int CategoryCode;
+        public string CharacterClass;
+        public int ItemTier = 3;
+        public string ItemGrade = "고대";
+        public int PageNo = 1;
+        public string SortCondition = "ASC";
+
+    }
+}
